b7bb7c57fa65cc99c8332f228f7e05ed5d2046c1
04637 <04637@163.com>
2020-10-14 01:16:13 +0800

    1. 移除jquery, 改用原生js, 防止加载失败 2. 优化错误处理, 减少插件报错 3. 添加右侧面板关闭按钮 4. 放宽xhr请求的判断, 包含content-type为application/json的请求

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,23 @@
-## todo
-1. 去除测试行 111 ✔
-2. 优化请求参数, 请求body, 响应体使之更易读 ✔
-3. json formatter 样式优化
-4. 完善manifest.json ✔
-5. 只显示xhr请求
+## Release notes
+### v0.1.3
+1. 移除jquery, 改用原生js, 防止加载失败
+2. 优化错误处理, 减少插件报错
+3. 添加右侧面板关闭按钮
+4. 放宽xhr请求的判断, 包含content-type为application/json的请求
+
+
+## 安装说明
+1. 进入chrome扩展程序页面 [chrome://extensions/](chrome://extensions/)
+2. 勾选开发者模式
+3. 点击左上角加载已解压的扩展程序
+4. 选择解压后的showProxyTarget文件夹, 注意该文件夹不能删除
+
+## 使用说明
+1. F12中最后面会加一个 "ProxyTarget" 的标签, 可以点击它按住鼠标拖动将其移至前面
+2. 在ProxyTarget面板会显示XHR请求, 点击对应请求的Name即可看到请求详情
+3. 右侧tab说明:
+    -   headers 是头部信息
+    -   Preview 是json格式响应值
+    -   Response 是json格式文本响应值
+    -   QueryString 是get请求参数
+    -   PostData 是post请求体
--- js/devtools.js
+++ js/devtools.js
@@ -1,68 +1,73 @@
-
-chrome.devtools.panels.create('ProxyTarget', 'img/icon.png', 'mypanel.html', null);
+chrome.devtools.panels.create('ProxyTarget', 'img/icon.png', 'showProxyTarget.html', null);
 chrome.devtools.network.onRequestFinished.addListener(
-  function (request) {
-        log(JSON.stringify(request))
-        request.getContent((body)=> {
-              if(body){
-                    request.responseBody = JSON.parse(body);
-              }
-        });
+    function (request) {
+        log(JSON.stringify(request));
         var isXHR = false;
-        for(var i=0;i<request.request.headers.length;i++){
-              if(request.request.headers[i].name=='routeUrl'){
-                    request.request.url = request.request.headers[i].value;
-              }
-              if(request.request.headers[i].name == 'X-Requested-With' && request.request.headers[i].value == 'XMLHttpRequest') {
-                    isXHR = true;
-              }
+
+        for (var i = 0; i < request.response.headers.length; i++) {
+            if (request.response.headers[i].name.toLowerCase() === 'content-type'.toLowerCase() && request.response.headers[i].value.includes("application/json")) {
+                isXHR = true;
+            }
         }
-        //todo 解开
-        // if(isXHR) {
-              appendRequest(request);
-        // }
-  }
+
+        for (var i = 0; i < request.request.headers.length; i++) {
+            if (request.request.headers[i].name === 'routeUrl') {
+                request.request.url = request.request.headers[i].value;
+            }
+            if (request.request.headers[i].name.toLowerCase() === 'X-Requested-With'.toLowerCase() && request.request.headers[i].value === 'XMLHttpRequest') {
+                isXHR = true;
+            }
+        }
+
+        if (isXHR) {
+            request.getContent((body) => {
+                if (body) {
+                    request.responseBody = JSON.parse(body);
+                }
+            });
+            appendRequest(request);
+        }
+    }
 );
 
-function log(info) {
-      chrome.devtools.inspectedWindow.eval(
-        'console.log('+
-        info + ')'
-      );
-}
-
 var requestArray = [];
-var index=0;
+var index = 0;
+
 function appendRequest(request) {
-      var data = {};
-      data.headers = request.response.headers;
-      data.type = request.response.content.mimeType;
-      data.size = (request.response.content.size/1000).toFixed(1);
-      data.status = request.response.status;
-      data.time = request.time.toFixed(0);
-      data.name = request.request.url.substr(request.request.url.lastIndexOf("/")+1);
-      var $table = document.querySelector("#requestTable");
-      var $tr = document.createElement("tr");
-      $tr.className = 'data';
-      $tr.innerHTML = "<tr>" +
-        "<td class='clickName' index='"+index+"'>"+data.name+"</td>" +
-        "<td class='canHide'>"+data.status+"</td>" +
-        "<td class='canHide'>"+data.type+"</td>" +
-        "<td class='canHide'>"+data.size+" kB</td>" +
-        "<td class='canHide'>"+data.time+" ms</td>" +
+    var data = {};
+    data.headers = request.response.headers;
+    data.type = request.response.content.mimeType;
+    data.size = (request.response.content.size / 1000).toFixed(1);
+    data.status = request.response.status;
+    data.time = request.time.toFixed(0);
+    data.name = request.request.url.substr(request.request.url.lastIndexOf("/") + 1);
+    var $table = document.getElementById("requestTbody");
+    var $tr = document.createElement("tr");
+    $tr.className = 'data';
+    $tr.innerHTML = "<tr>" +
+        "<td class='clickName' index='" + index + "'>" + data.name + "</td>" +
+        "<td class='canHide'>" + data.status + "</td>" +
+        "<td class='canHide'>" + data.type + "</td>" +
+        "<td class='canHide'>" + data.size + " kB</td>" +
+        "<td class='canHide'>" + data.time + " ms</td>" +
         "</tr>";
-      $table.appendChild($tr)
-      requestArray.push(request);
-      index++;
-      var $tableContainer = document.querySelector("#tableContainer");
-      $tableContainer.scrollTop = $tableContainer.scrollHeight;
+    if ($table) {
+        $table.appendChild($tr);
+    }
+    $tr.childNodes[0].addEventListener("click", clickName, false);
+    requestArray.push(request);
+    index++;
+    var $tableContainer = document.getElementById("tableContainer");
+    if ($tableContainer) {
+        $tableContainer.scrollTop = $tableContainer.scrollHeight;
+    }
 }
 
 function renderJson(request) {
-      var $info = document.querySelector("#info");
-      var _html = "";
-      // General
-      _html += "<span class='subtitle'>▼ General</span><br>" +
+    var $info = document.getElementById("info");
+    var _html = "";
+    // General
+    _html += "<span class='subtitle'>▼ General</span><br>" +
         "<span class='subtitle indent'>Request URL: </span>" +
         request.request.url +
         "<br>" +
@@ -72,80 +77,123 @@ function renderJson(request) {
         "<span class='subtitle indent'>Status Code: </span>" +
         request.response.status +
         "<hr>";
-      // Response Headers
-      var responseHeaders = request.response.headers;
-      _html += "<span class='subtitle'>▼ Response Headers</span><br>";
-      for(var i=0;i<responseHeaders.length;i++) {
-            _html+="<span class='subtitle indent'>"+responseHeaders[i].name+": </span>" +
+    // Response Headers
+    var responseHeaders = request.response.headers;
+    _html += "<span class='subtitle'>▼ Response Headers</span><br>";
+    for (var i = 0; i < responseHeaders.length; i++) {
+        _html += "<span class='subtitle indent'>" + responseHeaders[i].name + ": </span>" +
             responseHeaders[i].value +
             "<br>"
-      }
-      _html += "<hr>";
-      // Request Headers
-      var requestHeaders = request.request.headers;
-      _html += "<span class='subtitle'>▼ Request Headers</span><br>";
-      for(var i=0;i<requestHeaders.length;i++) {
-            _html+="<span class='subtitle indent'>"+requestHeaders[i].name+": </span>" +
-              requestHeaders[i].value +
-              "<br>"
-      }
-      _html += "<hr>";
-      $info.innerHTML = _html;
-      var $queryString = document.querySelector("#queryString");
-      if(request.request.queryString.length>0) {
-            const queryString = new JSONFormatter(request.request.queryString, "Infinity");
-            $queryString.innerHTML = "";
-            $queryString.appendChild(queryString.render())
-      }
-      var $postData = document.querySelector("#postData");
-      if(request.request.postData){
-            const postData = new JSONFormatter(request.request.postData, "Infinity", {});
-            $postData.innerHTML = "";
-            $postData.appendChild(postData.render());
-            const postData2 = request.request.postData.text.replace(/\\"/g, '"').replace(/"{/g, "{")
-              .replace(/}"/, "}");
-            const postData3 = new JSONFormatter(JSON.parse(postData2), "Infinity");
-            $postData.appendChild(postData3.render());
-      }
-      var $responseBody = document.querySelector("#responseBody");
-      var $preview = document.querySelector("#preview");
-      if(request.responseBody) {
-            const responseBody = new JSONFormatter(request.responseBody);
-            $preview.innerHTML = "";
-            $preview.appendChild(responseBody.render());
-            $responseBody.innerHTML = JSON.stringify(request.responseBody, undefined, 4)
-      }
+    }
+    _html += "<hr>";
+    // Request Headers
+    var requestHeaders = request.request.headers;
+    _html += "<span class='subtitle'>▼ Request Headers</span><br>";
+    for (var i = 0; i < requestHeaders.length; i++) {
+        _html += "<span class='subtitle indent'>" + requestHeaders[i].name + ": </span>" +
+            requestHeaders[i].value +
+            "<br>"
+    }
+    _html += "<hr>";
+    $info.innerHTML = _html;
+    var $queryString = document.getElementById("queryString");
+    if (request.request.queryString.length > 0) {
+        const queryString = new JSONFormatter(request.request.queryString, "Infinity");
+        $queryString.innerHTML = "";
+        $queryString.appendChild(queryString.render())
+    }
+    var $postData = document.getElementById("postData");
+    if (request.request.postData) {
+        const postData = new JSONFormatter(request.request.postData, "Infinity", {});
+        $postData.innerHTML = "";
+        $postData.appendChild(postData.render());
+        const postData2 = request.request.postData.text.replace(/\\"/g, '"').replace(/"{/g, "{")
+            .replace(/}"/, "}");
+        const postData3 = new JSONFormatter(JSON.parse(postData2), "Infinity");
+        $postData.appendChild(postData3.render());
+    }
+    var $responseBody = document.getElementById("responseBody");
+    var $preview = document.getElementById("preview");
+    if (request.responseBody) {
+        const responseBody = new JSONFormatter(request.responseBody);
+        $preview.innerHTML = "";
+        $preview.appendChild(responseBody.render());
+        $responseBody.innerHTML = JSON.stringify(request.responseBody, undefined, 4)
+    }
 }
 
-if(document.readyState !== 'loading') {
-      eventHandler();
-}else {
-      document.addEventListener("DOMContentLoaded", eventHandler);
-}
+document.addEventListener("DOMContentLoaded", function () {
+    eventHandler();
+});
 
 function eventHandler() {
-      $("#clearBtn").on("click", function(){
-            $("#requestTable .data").remove();
-      });
-      $("#requestTable").on("click", ".clickName", function(){
-            $("#author").hide();
-            $(".clickName.active").removeClass("active");
-            $(this).addClass("active");
-            var $right = $("#right");
-            $("#requestTable").addClass("onlyName");
-            $("#left").width("25%");
-            $right.show();
-            var _index = Number($(this).attr("index"));
-            // const responseBody = new JSONFormatter({"a":{'b':"c"}});
-            // $("#info").append(responseBody.render());
-            renderJson(requestArray[_index])
-      });
-      $("#right").on("click", ".tabBtn", function() {
-            var _attr = $(this).attr("attr");
-            $(".tabBtn.active").removeClass("active");
-            $(this).addClass("active");
-            $(".tab").addClass("hide");
-            $("#"+_attr).removeClass("hide");
-      })
+    var clearBtnEl = document.getElementById("clearBtn");
+    if (clearBtnEl) {
+        clearBtnEl.addEventListener("click", clearTable, false);
+    }
+
+    var clickTabEls = document.getElementsByClassName("tabBtn");
+    if (clickTabEls) {
+        for (var i = 0; i < clickTabEls.length; i++) {
+            clickTabEls[i].addEventListener("click", clickTabBtn, false);
+        }
+    }
+
+    var closeBtn = document.getElementById("closeBtn");
+    if (closeBtn) {
+        closeBtn.addEventListener("click", closeRight, false);
+    }
 }
 
+function clearTable() {
+    var $requestTbody = document.getElementById("requestTbody");
+    $requestTbody.innerHTML = "";
+}
+
+function closeRight() {
+    document.getElementById("requestTable").classList.remove("onlyName");
+    document.getElementById("left").style.width = "100%";
+    document.getElementById("right").style.display = 'none';
+}
+
+function clickName(e) {
+    document.getElementById("author").style.display = 'none';
+    var clickNameEls = document.getElementsByClassName("clickName");
+    if (clickNameEls) {
+        for (var i = 0; i < clickNameEls.length; i++) {
+            clickNameEls[i].classList.remove("active");
+        }
+    }
+    e.target.classList.add("active");
+
+    document.getElementById("requestTable").classList.add("onlyName");
+    document.getElementById("left").style.width = "25%";
+    document.getElementById("right").style.display = 'block';
+    var _index = Number(e.target.getAttribute("index"));
+    renderJson(requestArray[_index]);
+}
+
+function clickTabBtn(e) {
+    var _attr = e.target.getAttribute("attr");
+    var tabBtnEls = document.getElementsByClassName("tabBtn");
+    if (tabBtnEls) {
+        for (var i = 0; i < tabBtnEls.length; i++) {
+            tabBtnEls[i].classList.remove("active");
+        }
+    }
+    e.target.classList.add("active");
+    var tabEls = document.getElementsByClassName("tab");
+    if (tabEls) {
+        for (var i = 0; i < tabEls.length; i++) {
+            tabEls[i].classList.add("hide");
+        }
+    }
+    document.getElementById(_attr).classList.remove("hide");
+}
+
+function log(info) {
+    chrome.devtools.inspectedWindow.eval(
+        'console.log(' +
+        info + ')'
+    );
+}
